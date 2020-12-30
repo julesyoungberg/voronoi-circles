3338cb3f749496f8b60869afae86a4dc08b13ebb
Jules Youngberg <julesdayoungberg@gmail.com>
2020-12-29 17:36:43 -0800

    basic veroni circles

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     nannou::sketch(view).size(SIZE, SIZE).run();
 }
 
-fn map_veroni_point(point: Vector2<f64>) -> Vector2 {
+fn map_veroni_point(point: &Vector2<f64>) -> Vector2 {
     let res = veroni::GRID_RES as f32;
     let size = SIZE as f32;
     let normalized = pt2(point.x as f32 / res, point.y as f32 / res);
@@ -18,15 +18,28 @@ fn map_veroni_point(point: Vector2<f64>) -> Vector2 {
     );
 }
 
+fn scale_veroni_value(v: f64) -> f32 {
+    let size = SIZE as f64;
+    let normalized = v / veroni::GRID_RES as f64;
+    let mapped = normalized * size;
+    return mapped as f32;
+}
+
 fn view(app: &App, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
 
     let points = veroni::get_points();
-    for row in points {
-        for point in row {
-            let mapped = map_veroni_point(point);
-            draw.ellipse().xy(mapped).color(STEELBLUE);
+    let radiuses = veroni::get_radiuses(&points);
+    for y in 0..veroni::GRID_RES {
+        for x in 0..veroni::GRID_RES {
+            let mapped = map_veroni_point(&points[x as usize][y as usize]);
+            let radius = scale_veroni_value(radiuses[x as usize][y as usize]) / 2.0;
+
+            draw.ellipse()
+                .xy(mapped)
+                .wh(pt2(radius, radius))
+                .color(STEELBLUE);
         }
     }
 
--- src/veroni.rs
+++ src/veroni.rs
@@ -1,6 +1,6 @@
 use nannou::prelude::*;
 
-pub static GRID_RES: i32 = 8;
+pub static GRID_RES: usize = 8;
 
 // classic GLSL-style random vector generator
 fn rand2(c: Vector2<f64>) -> Vector2<f64> {
@@ -10,18 +10,58 @@ fn rand2(c: Vector2<f64>) -> Vector2<f64> {
     return pt2(p.x.abs(), p.y.abs());
 }
 
-// generates a grid of random points
+// generate a grid of random points
 pub fn get_points() -> Vec<Vec<Vector2<f64>>> {
-    let size = GRID_RES as usize;
-    let mut points = vec![vec![pt2(0.0 as f64, 0.0 as f64); size]; size];
+    let mut points = vec![vec![pt2(0.0 as f64, 0.0 as f64); GRID_RES]; GRID_RES];
 
     for y in 0..GRID_RES {
         for x in 0..GRID_RES {
             let bin_coord = pt2(x as f64, y as f64);
             let point = bin_coord + rand2(bin_coord);
-            points[x as usize][y as usize] = point;
+            points[x][y] = point;
         }
     }
 
     return points;
+}
+
+// compute the maximum radius for each point such that the circle is contained in the cell
+pub fn get_radiuses(points: &Vec<Vec<Vector2<f64>>>) -> Vec<Vec<f64>> {
+    let mut radiuses = vec![vec![0.0 as f64; GRID_RES]; GRID_RES];
+
+    for y in 0..GRID_RES {
+        for x in 0..GRID_RES {
+            let center_point = points[x][y];
+            let mut radius: f64 = 0.0;
+
+            // search neighbors for nearest
+            for yoff in 0..2 {
+                let yoffset = yoff - 1;
+
+                for xoff in 0..2 {
+                    let xoffset = xoff - 1;
+
+                    if yoffset == 0 && xoffset == 0 {
+                        continue;
+                    }
+
+                    let nx = (x as i32 + xoffset) % GRID_RES as i32;
+                    let ny = (y as i32 + yoffset) % GRID_RES as i32;
+                    let neighbor = points[nx.abs() as usize][ny.abs() as usize];
+
+                    let diff = center_point - neighbor;
+                    let hyp = diff.x * diff.x + diff.y * diff.y;
+                    let dist = hyp.sqrt();
+
+                    if radius == 0.0 || dist < radius {
+                        radius = dist;
+                    }
+                }
+            }
+
+            radiuses[x as usize][y as usize] = radius;
+        }
+    }
+
+    return radiuses;
 }
